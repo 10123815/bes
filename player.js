79792792e6ccfaac9015306cb80e148a9e62b330
10123815 <ysdysdysdysd123@163.com>
2016-05-23 22:26:34 +0800

    accelerate & decelerate

--- player.js
+++ player.js
@@ -1,32 +1,53 @@
+/**
+ * @fileoverview Player class represent a player in this game.
+ * @author ysd
+ */
+
+
 var common = require('./common');
 var define = require('./define');
 
+/**
+ * Player actions and properties.
+ * @param {string} name Player's name given by user.
+ */
 function Player(name) {
 
-	// information of the player
-	this.id = allocateId()
+	/**
+	 * Player's id.
+	 */
+	this.id = 0
+
+	/**
+	 * Player's name.
+	 */
 	this.usnm = name
 
-	// the radius 0f the circle
-	// it need to sync to another client
+	/**
+	 * Radius of the circle. It need to be synchronized to all clients.
+	 */
 	this.radius = 1
 
-	// the center of the circle
-	// it need to sync to another client 
+	/**
+	 * Center of the circle. It need to be synchronized to all clients.
+	 */
 	this.position = new common.Vector2(0, 0);
 
-	// 1 m/s
+	/**
+	 * Player's move speed per second.
+	 */
 	this.speed = 1;
 
 }
 
 /**
  * Player move to a point.
- * @param destination {Vector2} Destination point.
+ * @param {Vector2} destination Destination point.
  */
 Player.prototype.moveTo = function (destination) {
 
 	var movement = new Vector2(0, 0);
+	var spd = this.speed * define.DELTA_TIME;
 
 	this.doMoveTo = function (destination) {
 		// Direction and distance.
@@ -34,30 +55,29 @@ Player.prototype.moveTo = function (destination) {
 		var distance = dir.length();
 		dir.x /= distance;
 		dir.y /= distance;
+		var targetMovement = new Vector2(dir.x * spd, dir.y * spd);
 
-		if (distance > this.speed) {
-
-			// move
-			var spd = this.speed * define.DELTA_TIME;
-			var targetMovement = new Vector2(dir.x * spd, dir.y * spd);
-			movement = common.vector2Lerp(movement, targetMovement, 0.8);
-
+		if (distance > 0.1) {
+			
+			if (distance > this.speed) {
+				// accelarate
+				movement = common.vector2Lerp(movement, targetMovement, 0.5);
+			} else {
+				// decelarate
+				movement = common.vector2Lerp(movement, common.Zero2);
+			}
+			
 			// update position
 			this.position = common.vector2Add(this.position, movement);
 
-			// TODO : boardcast new position to all clients to tell clients the position after 1 second
+			// TODO : boardcast new position to all clients to tell clients the position after 0.1 second
 
 			// schedule the next move after 1 second
 			setTimeout(function () {
 				this.doMoveTo(destination);
 			}, 100);
-
-		}
-		else if (distance < this.speed && distance > 0.1) {
-			// closed to the destination, decrease the speed
-			// TODO : 
-		}
-		else {
+			
+		} else {
 			// reach the destination
 			delete this.movement;
 		}
@@ -66,18 +86,5 @@ Player.prototype.moveTo = function (destination) {
 	this.doMoveTo(destination);
 
 }
-
-// Player.prototype.updatePosition = function (x, y) {
-// 	this.position.x = x;
-// 	this.position.y = y;
-
-// 	// TODO : boardcast position to all clients in AOI
-// }
-
-// Player.prototype.updateRadius = function (r) {
-// 	this.radius = r;
-
-// 	// TODO : broadcast radiu to all clients in AOI
-// }
 
 exports.Player = Player
