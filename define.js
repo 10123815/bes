0d1a6785bc76df377ad5b0cc243450fb0f5ee57b
10123815 <ysdysdysdysd123@163.com>
2016-05-23 22:40:49 +0800

    .

---
+++ define.js
@@ -0,0 +1,10 @@
+/**
+ * @fileoverview Some constances defination.
+ * @author ysd
+ */
+
+/** @define {Number} Max player number of the server. */
+exports.MAX_PLAYER = 1000;
+
+/** #define {Number} Simulated frequency. */
+exports.DELTA_TIME = 0.1;
