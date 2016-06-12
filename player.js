6c942b887c8df465868bb4c54375e1cd7f3c4f96
10123815 <ysdysdysdysd123@163.com>
2016-06-12 22:03:46 +0800

    move

--- player.js
+++ player.js
@@ -5,6 +5,7 @@
 
 var common = require('./common');
 var define = require('./define');
+var Vector2 = require('./common').Vector2;
 
 /**
  * Player actions and properties.
@@ -35,17 +36,20 @@ function Player(name) {
 	/**
 	 * Player's move speed per second.
 	 */
-	this.speed = 1;
-	
+	this.speed = 5;
+
 	/**
 	 * Init fielf of aoi. It must big than the field of view by speed.
 	 */
 	this.scope = [16, 9];
 
+	this.movement = new Vector2(0, 0);
+
 }
 
 Player.prototype.eat = function (foodRadius, onPlayerEat) {
 	// Grow when player eat another player.
+	// And speed will descend.
 }
 
 /**
@@ -54,44 +58,47 @@ Player.prototype.eat = function (foodRadius, onPlayerEat) {
  */
 Player.prototype.moveTo = function (destination, onPlayerMove) {
 
-	var movement = new Vector2(0, 0);
-	var spd = this.speed * define.DELTA_TIME;
+	playerMove(this, destination, onPlayerMove);
 
-	this.doMoveTo = function (destination) {
-		// Direction and distance.
-		var dir = common.vector2Minus(destination, this.position);
-		var distance = dir.length();
-		dir.x /= distance;
-		dir.y /= distance;
-		var targetMovement = new Vector2(dir.x * spd, dir.y * spd);
+}
 
-		if (distance > 0.1) {
-			
-			if (distance > this.speed) {
-				// accelerate
-				movement = common.vector2Lerp(movement, targetMovement, 0.5);
-			} else {
-				// decelerate
-				movement = common.vector2Lerp(movement, common.Zero2);
-			}
-			
-			// update position
-			this.position = common.vector2Add(this.position, movement);
-			onPlayerMove(this.id, this.position);
-			
-			// schedule the next move after 1 second
-			setTimeout(function () {
-				this.doMoveTo(destination);
-			}, 100);
-			
+function playerMove(player, destination, onPlayerMove) {
+
+	var spd = player.speed * define.DELTA_TIME;
+
+	// Direction to the destination
+	var dir = common.vector2Minus(destination, player.position);
+	var distance = dir.length();
+	dir.x /= distance;
+	dir.y /= distance;
+
+	// Lerp to player vector.
+	var targetMovement = common.vector2Multi(dir, spd);
+
+	if (distance > 0.1) {
+
+		if (distance > spd) {
+			// accelerate
+			player.movement = common.vector2Lerp(player.movement, targetMovement, 0.5);
 		} else {
-			// reach the destination
-			delete this.movement;
+			// decelerate
+			player.movement = common.vector2Lerp(player.movement, common.Zero, 0.5);
 		}
+
+		// update position
+		player.position = common.vector2Add(player.position, player.movement);
+		onPlayerMove(player.id, player.position);
+
+
+		// schedule the next move after 1 second
+		setTimeout(function () {
+			playerMove(player, destination, onPlayerMove);
+		}, define.DELTA_TIME * 1000);
+
 	}
-
-	this.doMoveTo(destination);
-
+	else {
+		return;
+	}
 }
 
 exports.Player = Player
